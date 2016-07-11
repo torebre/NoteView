fdf62f9fe2b752f0c3604f61cc2f8badcad4848f
Tore Brede <tore.brede@gmail.com>
2016-07-11 14:29:03 +0200

    Adding a gulp file for copying the HTML to the build directory.

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var wiredep = require('wiredep').stream;
+var gulpBowerFiles = require('main-bower-files');
+
+
+gulp.task('default', function() {
+// TODO
+});
+
+gulp.task('bower', function () {
+    gulp.src('./src/*.html')
+    .pipe(wiredep({'ignorePath': '../../../bower_components/'}))
+        .pipe(gulp.dest('./build'));
+});
+
+gulp.task("bower-files", function() {
+    gulp.src(gulpBowerFiles()).pipe(gulp.dest("./build"));
+});
+
+gulp.task("copy-html", function() {
+    gulp.src("./html/*.html")
+        .pipe(gulp.dest("./build"));
+});
+
+// gulp.task("copy-css", function() {
+//     gulp.src('./src/main/html/app.css').pipe(gulp.dest('./build'));
+// })
+
+
+gulp.task('build', ['bower-files', 'bower']);
